404f54898426167a8e599dd3d25ea24113958321
haakaashs <haakaash10@gmail.com>
2024-01-25 17:39:46 +0900

    added rest end points for communicating with UI

---
+++ client/grpc_services/rest.go
@@ -0,0 +1,25 @@
+package grpcservices
+
+import (
+	"net/http"
+
+	"github.com/haakaashs/cloudbee/protos/blog"
+)
+
+var Client blog.BlogServiceClient
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ReadPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+
+}
