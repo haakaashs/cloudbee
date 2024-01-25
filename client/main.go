af63bc7f9acb3b92f0e8a455c5c6454969b9c23d
haakaashs <haakaash10@gmail.com>
2024-01-25 17:37:22 +0900

    added client side code

---
+++ client/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	grpcservices "github.com/haakaashs/cloudbee/client/grpc_services"
+	"github.com/haakaashs/cloudbee/protos/blog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	url  = "localhost:50001"
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("connection to server failed %v", err)
+	}
+
+	defer conn.Close()
+
+	grpcservices.Client = blog.NewBlogServiceClient(conn)
+	// we can directly hit the server using client
+
+	// or we can indirectly hit the server using client using rest API
+	server := mux.NewRouter()
+
+	server.HandleFunc("/blog", grpcservices.CreatePost).Methods("POST")
+	server.HandleFunc("/blog/{id}", grpcservices.ReadPost).Methods("GET")
+	server.HandleFunc("/blog", grpcservices.UpdatePost).Methods("PUT")
+	server.HandleFunc("/blog/{id}", grpcservices.DeletePost).Methods("DELETE")
+
+	if err = http.ListenAndServe(port, server); err != nil {
+		log.Fatalf("error starting rest web server at port %s", port)
+	}
+}
